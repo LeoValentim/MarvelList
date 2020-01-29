b94f8520986d2ed4db7661fe675c865464361f51
Leo Valentim <leo.valent@hotmail.com>
2020-01-29 18:40:06 -0300

    Character details scene

---
+++ MarvelList/Scenes/CharacterDetails/Entity/CharacterDetailsEntity.swift
@@ -0,0 +1,9 @@
+//
+//  CharacterDetailsEntity.swift
+//  MarvelList
+//
+//  Created by resource on 29/01/20.
+//  Copyright © 2020 Leo Valentim. All rights reserved.
+//
+
+import Foundation
---
+++ MarvelList/Scenes/CharacterDetails/View/CharacterDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterDetailsViewController.swift
+//  MarvelList
+//
+//  Created by resource on 28/01/20.
+//  Copyright © 2020 Leo Valentim. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
